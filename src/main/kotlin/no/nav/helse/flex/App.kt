73f25f8cf055d1f2982862bde78fc3396bb0dc61
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-07-10 18:02:43 +0200

    Kan gjøre kall til slack

--- src/main/kotlin/no/nav/helse/flex/App.kt
+++ src/main/kotlin/no/nav/helse/flex/App.kt
@@ -1,5 +1,15 @@
 package no.nav.helse.flex
 
+import no.nav.helse.flex.slack.SlackClient
+
 fun main() {
+    val slackToken = System.getenv("SLACK_TOKEN")
+    val testchannel = System.getenv("TEST_CHANNEL")
+    if (slackToken == null) {
+        println("SLACK_TOKEN is not set")
+        return
+    }
+    val slackClient = SlackClient(slackToken)
+    slackClient.postMessage("hei", testchannel)
     println("hei")
 }
